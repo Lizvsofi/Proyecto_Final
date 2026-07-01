0fc73cf76f6f4f5c94b5f899ce6057f60884659f
Michelle Moran <michelle.moran@cua.uam.mx>
2026-07-01 14:39:22 -0600

    Agregar API REST de nodo A

---
+++ nodo_a/src/main/java/mx/uam/cua/nodo_a/FragmentController.java
@@ -0,0 +1,31 @@
+package mx.uam.cua.nodo_a;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/videos")
+public class FragmentController {
+
+    @GetMapping("/{video_prueba01.mp4}")
+    public ResponseEntity<Resource> getFragment(@PathVariable String name) throws Exception {
+
+        Path path = Paths.get("videos", name);
+
+        Resource resource = new UrlResource(path.toUri());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+}
