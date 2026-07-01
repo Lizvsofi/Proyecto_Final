8c823759b344eac9f44db26bb4cc50d87de5f790
Lizeth <lizeth.vazquez@cua.uam.mx>
2026-07-01 12:05:02 -0600

    registryservice

---
+++ registry_service/src/test/java/mx/uam/cua/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uam.cua.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
