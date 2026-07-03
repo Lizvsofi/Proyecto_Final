1a8da3083d05508a32c231bc4a53df394acb8e0a
Kigo524 <kigocode@gmail.com>
2026-07-03 15:38:50 -0600

    Configurar FragmentController en C

--- nodo_c/src/main/java/mx/uam/cua/nodo_c/controller/FragmentController.java
+++ nodo_c/src/main/java/mx/uam/cua/nodo_c/controller/FragmentController.java
@@ -1,12 +1,17 @@
 package mx.uam.cua.nodo_c.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/videos")
@@ -21,5 +26,20 @@ public class FragmentController {
                 nombre.endsWith(".mp4"));
 
         return Arrays.asList(archivos);
+    }
+
+    @GetMapping("/{nombre}")
+    public ResponseEntity<Resource> descargarVideo(
+            @PathVariable String nombre) throws IOException {
+
+        Path ruta = Paths.get("nodo_c", "videos")
+                .resolve(nombre);
+
+        Resource resource = new UrlResource(ruta.toUri());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + nombre + "\"")
+                .body(resource);
     }
 }
